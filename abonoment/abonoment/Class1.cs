5fb7a86a0026fcdf2b2c8b4b3304ea26effee43e
RobinLind <robin_flaks@hotmail.com>
2017-12-12 14:09:18 +0100

    legger til c# prosjektet

---
+++ abonoment/abonoment/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abonoment
+{
+    using System.Data.SqlClient;
+    using System.Data;
+    class MainClass
+    {
+        static void Main()
+        {
+            // Create the connection to the resource!
+            // This is the connection, that is established and
+            // will be available throughout this block.
+            String connectionString = "Server=[localhost:3306];Database=[linrob16_kolonial];Trusted_Connection=true";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Create the connectionString
+                // Trusted_Connection is used to denote the connection uses Windows Authentication
+                conn.Open();
+                if(conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("hei");
+                }
+                Console.WriteLine("bai");
+                conn.Close();
+            }
+        }
+    }
+}
