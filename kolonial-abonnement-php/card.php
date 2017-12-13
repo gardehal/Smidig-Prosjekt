6421f767f178d6b4f3052543a0e44128b822630c
grdall <garale16@student.westerdals.no>
2017-12-13 13:54:51 +0100

    Dropdown meny for levering, oppdatert database
    Slettet abonnement table, lastet opp konolial-abonnement datbase

--- kolonial-abonnement-php/card.php
+++ kolonial-abonnement-php/card.php
@@ -9,31 +9,37 @@
     
     <div id="select">
         <form class="" action="update-subscription.php?id=<?= $event['kunde_id'] ?>" method="post">
-            <!--- Dropdown tid --->
-            <select name="leveringstid">
-                <!-- TODO: enum eller selected shit = shit --->
-                <option value="<?= $event['leverings_tidspunkt'] ?>" selected><?= $event['leverings_tidspunkt'] ?></option>
-                <option value="07-09">07-09</option>
-                <option value="08-10">08-10</option>
-                <option value="08-11">08-11</option>
-                <option value="09-10">09-10</option>
-                <option value="09-11">09-11</option>
-                <option value="09-12">09-12</option>
-                <option value="09-14">09-14</option>
-                <option value="14-16">14-16</option>
-                <option value="16-19">16-19</option>
-            </select>
+            <div id="dropdown-order">
+                <button type="button" id="dropdown-btn" onclick="toggleDropdown()">Endre levering</button>
+                
+                <div id="dropdown-content">
+                    <!--- Dropdown tid --->
+                    <select name="leveringstid">
+                        <option value="<?= $event['leverings_tidspunkt'] ?>" selected><?= $event['leverings_tidspunkt'] ?></option>
+                        <option value="07-09">07-09</option>
+                        <option value="08-10">08-10</option>
+                        <option value="08-11">08-11</option>
+                        <option value="09-10">09-10</option>
+                        <option value="09-11">09-11</option>
+                        <option value="09-12">09-12</option>
+                        <option value="09-14">09-14</option>
+                        <option value="14-16">14-16</option>
+                        <option value="16-19">16-19</option>
+                    </select>
 
-            <!--- Intervall --->
-            <input type="number" id="intervall" name="intervall" value="<?= $event['intervall'] ?>">
+                    <!--- Intervall --->
+                    <input type="number" id="intervall" name="intervall" value="<?= $event['intervall'] ?>">
 
-            <!--- Leveringsdato --->
-            <input type="date" id="leveringsdato" name="leveringsdato" value="<?= $event['leverings_dato'] ?>">
-
-            <!--- Oppdater dato etc. --->
-            <button type="button" id="update-list-btn" name="update-list-btn" onclick="submit();">Oppdater</button>
-            <!--- Kjø lsiten (burde gå til Handlekurv) --->
-            <button type="button" id="buy-list-btn" name="buy-list-btn">Kjøp listen</button>
+                    <!--- Leveringsdato --->
+                    <input type="date" id="leveringsdato" name="leveringsdato" value="<?= $event['leverings_dato'] ?>">
+                    
+                    <!--- Oppdater dato etc. --->
+                    <button type="button" id="update-list-btn" onclick="submit();">Oppdater</button>
+                </div>
+            </div>
+                
+            <!--- Kjøp listen (burde gå til Handlekurv) --->
+            <button type="button" id="buy-list-btn">Kjøp listen</button>
 
 
             <input type="hidden" name="edit_kunde_id" value="<?= $event['kunde_id'] ?>"> 
@@ -41,3 +47,18 @@
         </form>
     </div>
 </div>
+
+<script>
+    function toggleDropdown()
+        {
+            var x = document.getElementById("dropdown-content");
+            if (x.style.display === "block") 
+            {
+                x.style.display = "none";
+            } 
+            else 
+            {
+                x.style.display = "block";
+            }
+        }
+</script>
