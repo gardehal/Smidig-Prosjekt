1b56d9221c9153606df57e298f2483c66a724e9a
RobinLind <robin_flaks@hotmail.com>
2017-12-13 11:18:33 +0100

    small things

--- abonoment/abonoment/Class1.cs
+++ abonoment/abonoment/Class1.cs
@@ -15,8 +15,8 @@ namespace abonoment
             // Create the connection to the resource!
             // This is the connection, that is established and
             // will be available throughout this block.
-            Console.WriteLine("bai");
-            String connectionString = "Server=[localhost:3306];Database=[linrob16_kolonial];Trusted_Connection=true";
+            Console.WriteLine("check");
+            String connectionString = "Server=[localhost];Database=[abonnement];Trusted_Connection=true";
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 // Create the connectionString
@@ -24,9 +24,9 @@ namespace abonoment
                 conn.Open();
                 if(conn.State == ConnectionState.Open)
                 {
-                    Console.WriteLine("hei");
+                    Console.WriteLine("connection!");
                 }
-                Console.WriteLine("bai");
+                Console.WriteLine("over");
                 conn.Close();
             }
         }
