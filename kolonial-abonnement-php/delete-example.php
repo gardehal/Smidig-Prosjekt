dbd9eedfe378c39f0904f51674565b79724f79e2
grdall <garale16@student.westerdals.no>
2017-12-15 12:42:10 +0100

    Gjort klart for header + søkefunksjon

--- kolonial-abonnement-php/delete-example.php
+++ kolonial-abonnement-php/delete-example.php
@@ -1,5 +1,5 @@
 <?php
-require 'connection.php';
+    require 'connection.php';
 
     //todo_id, todo_check, todo_todo
     $statement1 = $connection->prepare('DELETE FROM abonnement WHERE kunde_id = 0 AND liste_id = 0');
---
+++ kolonial-abonnement-php/search-results.php
@@ -0,0 +1,29 @@
+<?php
+    require 'connection.php';
+    $getsearch = $_GET['search'];
+    $statement1 = $connection->prepare('SELECT * FROM abonnement 
+    WHERE kunde_id = "'.$getsearch.'"
+    ORDER BY kunde_id DESC');
+    require 'statement-execute-1.php';
+
+    //require 'header.php';
+?>
+
+<h1> Viser resultater for <?= $getsearch ?> </h1>
+
+<?php
+    foreach ($events1 as $event) 
+    {   
+        require 'card.php';
+        echo "<br>";
+        $searchcounter++;
+    } 
+    if($searchcounter == 0)
+    {
+        echo "Beklager! Vi fant ingen artikkler med $getsearch!";
+    }
+?>
+
+<?php
+    //require 'footer.php';
+?>
