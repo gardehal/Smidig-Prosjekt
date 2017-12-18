306c809002fbcfb8906a294e019984d1c7499570
RobinLind <robin_flaks@hotmail.com>
2017-12-18 14:41:31 +0100

    tar med liste_id igjen

--- Nettsiden/kolonial-abonnement-php/crud/cards/subscription-card.php
+++ Nettsiden/kolonial-abonnement-php/crud/cards/subscription-card.php
@@ -2,7 +2,7 @@
     <div id="display">
         <p>
             Kunde ID: <?= $event['kunde_id'] ?> |
-            Liste ID: <?= $event['liste_navn'] ?> |
+            <?= $event['liste_navn'] ?> <?= $event['liste_id'] ?> |
             Leveringsdato: <?= $event['leverings_dato'] ?>
         </p>
     </div>
