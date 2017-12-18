0978a318a28bc07d13a7c1debc7d70711c9de2e8
RobinLind <robin_flaks@hotmail.com>
2017-12-18 14:50:07 +0100

    comment

--- Nettsiden/kolonial-abonnement-php/crud/cards/subscription-card.php
+++ Nettsiden/kolonial-abonnement-php/crud/cards/subscription-card.php
@@ -2,7 +2,7 @@
     <div id="display">
         <p>
             Kunde ID: <?= $event['kunde_id'] ?> |
-            <?= $event['liste_navn'] ?> <?= $event['liste_id'] ?> |
+            <?= $event['liste_navn'] ?> <?= $event['liste_id'] ?> |//endre "liste_id" til abonement.liste_id hvis dette ikke funker.
             Leveringsdato: <?= $event['leverings_dato'] ?>
         </p>
     </div>
